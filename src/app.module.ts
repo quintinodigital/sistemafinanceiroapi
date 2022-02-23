3bf8d04881f9c4031ca95077b4817660f1852dd6
Desenvolvimento <repository.desenvolvimento@gmail.com>
2022-02-23 18:09:50 -0300

    SISTEMAFINANCEIROAPI-23022022175901
    Funcionalidade de Gestão Faturas e Mensalidades Contratuais

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,9 @@ import { AppService } from './app.service';
 import { CartaoBancarioModule } from './module/cartao-bancario.module';
 import { ContaBancariaModule } from './module/conta-bancaria.module';
 import { ContratoModule } from './module/contrato.module';
+import { FaturaMensalidadeContratualModule } from './module/fatura-mensalidade-contratual.module';
 import { PessoaModule } from './module/pessoa.module';
 import { UsuarioModule } from './module/usuario.module';
-import { FaturaMensalidadeContratualModule } from './module/fatura-mensalidade-contratual.module';
 
 @Module({
   imports: [
